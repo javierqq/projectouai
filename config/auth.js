86a138c75b8b470b95fa69a7e1e4995b4576c330
Javier Quiroz <javierqq@gmail.com>
2017-06-13 14:11:32 -0400

    add facebook id

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports={
+  facebookAuth: {
+    clientID    :     "446654559035125",
+    clientSecret   :     "b4003dd69179f11e91ef814d4b35b28a",
+    callbackURL          :     "http://localhost:3000/auth/facebook/callback",
+  }
+}
