454e8d155dfde74a5c02d777774b2ea37936cb6c
Javier Quiroz <javierqq@gmail.com>
2017-06-13 14:18:24 -0400

    add facebook auth

---
+++ models/user.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+
+var userSchema = mongoose.Schema({
+  local: {
+    name: String,
+    email: String,
+    password: String,
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+    username: String,
+  },
+});
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
