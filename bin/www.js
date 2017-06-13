311f558aa22fe33e21345e3b71124535129c908f
Javier Quiroz <javierqq@gmail.com>
2017-06-13 14:06:32 -0400

    add passport use

--- bin/www.js
+++ bin/www.js
@@ -83,36 +83,6 @@ io.on('connection', function (socket) {
         }
     });
 });
-//facebook login
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '446654559035125',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-// FB response
-   FB.getLoginStatus(function(response) {
-       statusChangeCallback(response);
-   });
-
-// FB callback check
-   function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
 
 /**
  * Listen on provided port, on all network interfaces.
